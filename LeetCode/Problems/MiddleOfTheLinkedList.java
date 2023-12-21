9a11cef506f29fe20985aa015cf0709958ce85b9
Bektur777 <ubei003@mail.ru>
2023-12-21 23:23:07 +0600

    Add problem solution Middle of the Linked List

---
+++ LeetCode/Problems/MiddleOfTheLinkedList.java
@@ -0,0 +1,37 @@
+package problems;
+
+/**
+ * Given the head of a singly linked list, return the middle node of the linked list.
+ *
+ * If there are two middle nodes, return the second middle node.
+ *
+ * Example 1:
+ *
+ * Input: head = [1,2,3,4,5]
+ * Output: [3,4,5]
+ * Explanation: The middle node of the list is node 3.
+ */
+
+public class MiddleOfTheLinkedList {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode middle = head;
+        ListNode end = head;
+
+        while (end != null && end.next != null) {
+            middle = middle.next;
+            end = end.next.next;
+        }
+
+        return middle;
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
