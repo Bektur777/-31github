9e195cb8da2f7093bf27937cd6240b3a76363cff
Bektur777 <ubei003@mail.ru>
2023-12-21 23:20:27 +0600

    Add problem solution Ransom Note

---
+++ LeetCode/Problems/RansomNote.java
@@ -0,0 +1,55 @@
+package problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+ *
+ * Each letter in magazine can only be used once in ransomNote.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: ransomNote = "a", magazine = "b"
+ * Output: false
+ * Example 2:
+ *
+ * Input: ransomNote = "aa", magazine = "ab"
+ * Output: false
+ * Example 3:
+ *
+ * Input: ransomNote = "aa", magazine = "aab"
+ * Output: true
+ */
+
+public class RansomNote {
+
+    public static void main(String[] args) {
+
+        System.out.println(canConstruct("aa", "aab"));
+
+    }
+
+    static boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            int currentCount = map.getOrDefault(c, 0);
+
+            if (currentCount == 0) {
+                return false;
+            }
+
+            map.put(c, currentCount - 1);
+        }
+
+        return true;
+    }
+
+}
